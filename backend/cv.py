eb8df88de814504ae4d9701e41653a9c320abb0d
Steven Mu <xsmu0922@gmail.com>
2022-04-25 00:31:15 -0400

    Delete cv.py

--- backend/cv.py
+++
@@ -1,9 +0,0 @@
-
-# capture image (with another method)
-# now capture image when front end says snap
-# process image (locate one color, multiple colors later)
-# return coordinates with the server back to processing
-
-
-
-
